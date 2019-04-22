ece0e05c575ef37b0c503f4ff91118150977d2d5
Elliot Tormey <elliot.tormey@gmail.com>
2019-04-22 20:49:41 +0100

    fix detekt issues

--- app/src/main/java/ie/otormaigh/x/danger/MainActivity.kt
+++ app/src/main/java/ie/otormaigh/x/danger/MainActivity.kt
@@ -3,8 +3,11 @@ package ie.otormaigh.x.danger
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 
+/**
+ * @author Elliot Tormey
+ * @since 2019-04-21
+ */
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
